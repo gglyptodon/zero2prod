66964df1b9f04f9423252986ba63d3b49283e6c7
gglyptodon <janina.mass@gmail.com>
2022-06-30 11:29:47 +0200

    add serde_derive, add response to post

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,29 @@
 use actix_web::dev::Server;
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
+use serde_derive::Deserialize;
+//#[get("/hello/{name}")]
+//pub async fn greet(name: web::Path<String>) -> impl Responder {
+//    format!("Hello {name}!")
+//}
 
-#[get("/hello/{name}")]
-pub async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
+#[derive(Deserialize)]
+struct Info{
+    name: String,
+    email: String
 }
+
 #[get("/health_check")]
 pub async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
+#[post("/subscriptions")]
+pub async fn subscribe(info: web::Form<Info>) -> impl Responder {
+    format!("Hello {}, with email: {} ",info.name, info.email )
+}
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().service(health_check))
+    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
         .listen(listener)?
         .run();
     Ok(server)
