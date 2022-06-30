849aa10ac39243431350cfdd1526ff9d1bc3fc76
Janina Mass <janina.mass@gmail.com>
2022-06-30 20:08:22 +0200

    add install instructions

---
+++ install_notes.md
@@ -0,0 +1,6 @@
+## ubuntu ##
+
+``sudo apt install postgresql-client``
+
+## cargo ##
+``cargo install sqlx-cli --no-default-features --features native-tls,postgres``
--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,6 +2,21 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v psql)" ]; then 
+  echo >&2 "not installed: `psql`. Install with your OS's pacakge manager. E.g. on ubuntu do: "; 
+echo >&2 "sudo apt install postgresql-client"
+  exit 1 
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then 
+  echo >&2 "not installed: `sqlx`. Install via cargo install:";
+  echo >&2 "    cargo install sqlx-cli --no-default-features --features native-tls,postgres";
+ 
+  exit 1 
+fi
+
+
+
 DB_USER=${POSTGRES_USER:=postgres}
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
