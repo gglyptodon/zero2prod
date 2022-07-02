edda1cd9ed8bc0ec4cf7b4dbf8a7521415af6734
Janina Mass <janina.mass@gmail.com>
2022-07-02 15:19:03 +0200

    add some comments

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -8,15 +8,21 @@ pub struct TestApp {
     pg_pool: PgPool,
     address: String,
 }
-
+/// spawns app at random port
+/// reads configuration file
+/// overwrites database name (from config file) to random uuid
+/// calls configure_db (which creates a new database and runs the migrations in .migrations on it)
+/// receives a connection pool for database connection
+/// creates a new server instance with the connection pool
+/// spawns the server and returns a TestApp with server address and the connection pool
 async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
 
     let mut configuration = get_config().expect("Failed to read config");
     configuration.database.database_name = uuid::Uuid::new_v4().to_string();
-    let connection_string = configuration.database.connection_string();
-    let connection_pool = configure_db(&configuration.database).await; //expect("Could not connect to db");
+    // let connection_string = configuration.database.connection_string();
+    let connection_pool = configure_db(&configuration.database).await;
 
     let server =
         zero2prod::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
@@ -27,6 +33,9 @@ async fn spawn_app() -> TestApp {
     }
 }
 
+/// Creates a database from config
+/// runs migrations defines in `./migrations`
+/// and returns a connection pool for the new db
 async fn configure_db(config: &DbSettings) -> PgPool {
     let mut connection = PgConnection::connect(&config.connection_string_without_db())
         .await
@@ -43,9 +52,11 @@ async fn configure_db(config: &DbSettings) -> PgPool {
         )
         .await
         .expect( format!("Failed to create db {}", &config.database_name).as_str());
+
     let connection_pool = PgPool::connect(&config.connection_string())
         .await
         .expect(format!("Cannot connect to {}", &config.connection_string()).as_str());
+
     sqlx::migrate!("./migrations")
         .run(&connection_pool)
         .await
