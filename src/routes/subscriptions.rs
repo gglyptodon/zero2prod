269838f7cef0989a5ad72f1191acc65c9794ea49
Janina Mass <janina.mass@gmail.com>
2022-07-05 10:24:07 +0200

    refactor

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,8 +1,8 @@
+use std::pin::Pin;
 use actix_web::{post, web, HttpResponse, Responder};
 use chrono::Utc;
 use serde_derive::Deserialize;
-use sqlx::PgPool;
-use tracing_futures::Instrument;
+use sqlx::{PgPool};
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -25,8 +25,15 @@ pub async fn subscribe(
     info: web::Form<Info>,
     connection_pool: web::Data<PgPool>,
 ) -> impl Responder {
-    let query_span = tracing::info_span!("Saving subscriber");
-    match sqlx::query!(
+    //let query_span = tracing::info_span!("Saving subscriber");
+    match insert_subscriber(&connection_pool, &info).await {
+        Ok(_) => HttpResponse::Ok(),
+        Err(_) => HttpResponse::InternalServerError(),
+    }
+}
+#[tracing::instrument(name="Saving new subscriber to db", skip(info, connection_pool))]
+pub async fn insert_subscriber(connection_pool: &PgPool, info: &Info) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#" INSERT INTO subscriptions (id, name, email, subscribed_at) VALUES ($1, $2, $3, $4);
       "#,
         Uuid::new_v4(),
@@ -34,14 +41,11 @@ pub async fn subscribe(
         info.email,
         Utc::now()
     )
-    .execute(connection_pool.get_ref())
-    .instrument(query_span)
+    .execute(Pin::new(connection_pool).get_ref())
     .await
-    {
-        Err(e) => {
-            tracing::error!("Error saving new subscriber record: {:?}", e);
-            HttpResponse::InternalServerError()
-        }
-        Ok(_) => HttpResponse::Ok(),
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query:{:?}", e);
+        e
+    })?;
+    Ok(())
 }
